e4f6208d3eb58e55c520f1e64a86e43320c4422b
Kumara002 <kumaramurugan002@gmail.com>
2023-02-16 13:17:29 +0530

    react url updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app=express();
 mongoose.set('strictQuery', true);
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://gilded-florentine-98215f.netlify.app');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
